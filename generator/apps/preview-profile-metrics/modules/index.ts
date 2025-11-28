9281f023a293baf211c4158749b3d2e1ffb52efc
ROG <luan.roger.2003@gmail.com>
2025-11-28 14:54:08 -0300

    chore: add left modules and env example

--- generator/apps/preview-profile-metrics/modules/index.ts
+++ generator/apps/preview-profile-metrics/modules/index.ts
@@ -1,5 +1,5 @@
-import { GitHubModule, IntroModule, Module } from "metrics-modules/modules";
+import { DumyModule, IntroModule, Module, WakatimeModule } from "metrics-modules/modules";
 
 export function initModules(debug: boolean): Module[] {
-  return [new IntroModule(debug), new GitHubModule(debug)];
+  return [new IntroModule(debug), new WakatimeModule(debug), new DumyModule(debug)];
 }
