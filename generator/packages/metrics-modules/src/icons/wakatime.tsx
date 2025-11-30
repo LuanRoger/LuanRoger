4439c3d9180bfb479a5dfc3c4d5c351aa6d60622
ROG <luan.roger.2003@gmail.com>
2025-11-30 10:16:28 -0300

    fix: icons size and colors

--- generator/packages/metrics-modules/src/icons/wakatime.tsx
+++ generator/packages/metrics-modules/src/icons/wakatime.tsx
@@ -5,6 +5,9 @@ export function wakatimeIcon() {
     <svg
       role="img"
       viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      fill="#666666"
       stroke="#666666"
       xmlns="http://www.w3.org/2000/svg"
     >
