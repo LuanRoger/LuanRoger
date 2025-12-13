a4f32e92275a49e4528d5d8ef93eb675e8d899b1
ROG <luan.roger.2003@gmail.com>
2025-12-13 10:43:08 -0300

    refactor: remove image persistence from renderer

--- generator/packages/metrics-modules/src/index.ts
+++ generator/packages/metrics-modules/src/index.ts
@@ -1,11 +1,3 @@
 import { Renderer } from "@takumi-rs/core";
-import { loadImage } from "./img-loader";
 
-export const renderer = new Renderer({
-  persistentImages: [
-    {
-      src: "github-profile.jpg",
-      data: await loadImage("./public/github-profile.jpg"),
-    },
-  ],
-});
+export const renderer = new Renderer();
